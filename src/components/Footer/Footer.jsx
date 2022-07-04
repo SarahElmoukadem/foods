db089320c9f1e646ade76cb930d78f9f8669202f
sarahelmoukadem <sara.mokadm@iqraaly.com>
2022-07-04 01:05:49 -0700

    add footer & header structure and styles

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,143 @@
-import React from 'react'
+import React from 'react';
+import { Container,Row,Col,ListGroup, ListGroupItem } from 'reactstrap';
+import logo from '../../assets/images/res-logo.png';
+import '../../styles/footer.css';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="footer">
+      <Container>
+        <Row>
+          <Col lg='3' md='4' sm='6'>
+          <div className="logo footer__logo text-start">
+                        <img src={logo} alt="logo" />
+                        <h5>Greek</h5>
+                        <p>
+                          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                           Eos, aliquid.
+                            Nostrum eum maxime quos veritatis unde incidunt exercitationem nulla nemo impedit.
+                        </p>
+                    </div>
+
+          </Col>
+
+          <Col lg='3' md='4' sm='6'>
+            <h5 className='footer__title'>
+              Delivery Time
+            </h5>
+
+            <ListGroup className='deliver__time-list'>
+              <ListGroupItem  className='delivery__time-item border-0 ps-0'>
+                <span>
+                Sunday - Thursday
+                </span>
+                <p>
+                  10.00am - 11:00pm
+                </p>
+              </ListGroupItem>
+
+              <ListGroupItem  className='delivery__time-item border-0 ps-0'>
+                <span>
+                Friday - Saturday
+                </span>
+                <p>
+                  Off day
+                </p>
+              </ListGroupItem>
+
+            </ListGroup>
+          </Col>
+
+
+          <Col lg='3' md='4' sm='6'>
+
+          <h5 className='footer__title'>
+              Contact
+            </h5>
+
+            <ListGroup className='deliver__time-list'>
+
+            <ListGroupItem  className='delivery__time-item border-0 ps-0'>
+                <span>
+                  Location: Tagmo3, Egypt
+                </span>
+              </ListGroupItem>
+
+              <ListGroupItem  className='delivery__time-item border-0 ps-0'>
+                <span>
+                Phone: 012354847651
+                </span>
+              </ListGroupItem>
+
+              <ListGroupItem  className='delivery__time-item border-0 ps-0'>
+                <span>
+                  Email: example@gmail.com
+                </span>
+              
+              </ListGroupItem>
+
+            </ListGroup>
+
+          </Col>
+
+
+          <Col lg='3' md='4' sm='6'>
+          <h5 className='footer__title'>
+              NewsLetter
+            </h5>
+            <p>
+              Subscribe our newsLetter
+            </p>
+            <div className="newsletter">
+              <input type="email" placeholder='Enter Your email'/>
+              <span>
+                <i className='ri-send-plane-line'></i>
+              </span>
+            </div>
+          </Col>
+        </Row>
+        <Row className='mt-5'>
+          <Col lg='6' md='6' className=''>
+            <p className='copyright__text'>
+              Copyright - 2022, website made by companyName. All Rights Reserved.
+            </p>
+          </Col>
+          <Col lg='6' md='6'>
+            <div className="social__links d-flex align-items-center gap-4 justify-content-end">
+              <p className='m-0'>
+                Follow:
+              </p>
+              <span>
+                {" "}
+                <Link to=''>
+                  <i className='ri-facebook-line'></i>
+                </Link>{" "}
+              </span>
+
+              <span>
+                <Link to=''>
+                  <i className='ri-github-line'></i>
+                </Link>
+              </span>
+
+              <span>
+                <Link to=''>
+                  <i className='ri-youtube-line'></i>
+                </Link>
+              </span>
+
+              <span>
+                <Link to=''>
+                  <i className='ri-linkedin-line'></i>
+                </Link>
+              </span>
+
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
   )
 }
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, {useRef} from 'react'
 import { Container } from 'reactstrap';
 import logo from '../../assets/images/res-logo.png';
 import { NavLink, Link } from 'react-router-dom';
+
+import '../../styles/header.css';
 
 const nav__links = [
     {
@@ -19,26 +21,49 @@ const nav__links = [
 ]
 
 const Header = () => {
+    const menuRef = useRef(null)
+    const toggleMenu = () => menuRef.current.classList.toggle('show__menu')
     return (
         <header className='header'>
             <Container>
-                <div className="nav__wrapper">
+                <div className="nav__wrapper d-flex align-items-center justify-content-between">
                     <div className="logo">
                         <img src={logo} alt="logo" />
                         <h5>Greek</h5>
                     </div>
 
-                    <div className="navigation">
-                        <div className="menu">
+                    <div className="navigation" ref={menuRef} onClick={toggleMenu}>
+                        <div className="menu d-flex align-items-center gab-5">
                             {
                                 nav__links.map((item,index) => (
-                                    <NavLink to={item.path} key={index}>
+                                    <NavLink 
+                                    
+                                    to={item.path} key={index}
+                                    className={(navClass) => navClass.isActive ? "active__menu" : ""}>
                                         {item.display}
                                     </NavLink>
                                 ))
                             }
                         </div>
                     </div>
+
+                    <div className='nav__right d-flex align-items-center gab-4'>
+                        <span className="cart__icon">
+                            <i className='ri-shopping-basket-line'></i>
+                            <span className="cart__badge">2</span>
+                        </span>
+
+                        <span className="user">
+                            <Link to='/login'>
+                                <i className='ri-user-line'></i>
+                            </Link>
+                        </span>
+
+                        <span className='mobile__menu' onClick={toggleMenu}>
+                            <i className='ri-menu-line'></i>
+                        </span>
+                    </div>
+
                 </div>
             </Container>
         </header>
