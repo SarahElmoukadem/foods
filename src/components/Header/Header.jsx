1d316289739c6ee7bf9f7984571d9e13f5443b61
sarahelmoukadem <sara.mokadm@iqraaly.com>
2022-07-04 19:41:21 -0700

    enhancement for Layout & header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,8 @@ import React, {useRef, useEffect} from 'react'
 import { Container } from 'reactstrap';
 import logo from '../../assets/images/res-logo.png';
 import { NavLink, Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { cartUiActions } from '../../store/shopping-cart/cartUiSlice';
 import '../../styles/header.css';
 
 const nav__links = [
@@ -28,8 +29,13 @@ const Header = () => {
     const menuRef = useRef(null);
     const headerRef = useRef(null);
     const totalQuantity = useSelector(state => state.cart.totalQuantity)
+    const dispatch = useDispatch()
+   
     const toggleMenu = () => menuRef.current.classList.toggle('show__menu')
   
+    const toggleCart = () => {
+        dispatch(cartUiActions.toggle())
+    }
 
     useEffect(() => {
         window.addEventListener('scroll', () => {
@@ -67,7 +73,7 @@ const Header = () => {
                     </div>
 
                     <div className='nav__right d-flex align-items-center gap-4'>
-                        <span className="cart__icon">
+                        <span className="cart__icon " onClick={toggleCart}>
                             <i className='ri-shopping-basket-line'></i>
                             <span className="cart__badge">
                                 {totalQuantity}
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,15 +2,21 @@ import React from 'react'
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import Routes from '../../routes/Routers';
-
+import Carts from '../UI/cart/Carts.jsx';
+import { useSelector } from 'react-redux';
 const Layout = () => {
+  const showCart = useSelector((state) => state.cartUi.cartIsVisible)
   return (
     <div>
-        <Header />
-        <div>
-            <Routes />
-        </div>
-        <Footer />
+      <Header />
+
+      {showCart && <Carts />
+      }
+
+      <div>
+        <Routes />
+      </div>
+      <Footer />
     </div>
   )
 }
