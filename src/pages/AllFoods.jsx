c6805c594a6d965449e9eac3f11ecede11b15088
sarahelmoukadem <sara.mokadm@iqraaly.com>
2022-12-28 15:38:24 +0200

    foods list page

--- src/pages/AllFoods.jsx
+++ src/pages/AllFoods.jsx
@@ -34,7 +34,7 @@ const AllFoods = () => {
       <section>
         <Container>
           <Row>
-            <Col lg='6' md='6' sm='6'>
+            <Col lg='6' md='6' sm='6' xs='12'>
               <div className="search__widget d-flex align-items-center justify-content-between w-50">
                 <input type='text' placeholder="I'm looking for...." value={searchItem}
                   onChange={e => setSearchItem(e.target.value)}
@@ -45,7 +45,7 @@ const AllFoods = () => {
               </div>
             </Col>
 
-            <Col lg='6' md='6' sm='6' className="mb-5">
+            <Col lg='6' md='6' sm='6' xs='12' className="mb-5">
               <div className="sorting__widget text-end">
                 <select className='w-50'>
                   <option >Default</option>
