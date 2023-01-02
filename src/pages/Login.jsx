483c72c9fba842c069a73a7c24b043d2ec752b66
sarahelmoukadem <sara.mokadm@iqraaly.com>
2023-01-02 17:05:13 +0200

    login - signup pages

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,55 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import { Link } from 'react-router-dom'
+import { Col, Container, Row } from 'reactstrap'
+import Helmet from '../components/Helmet/Helmet'
+import CommonSection from '../components/UI/common-section/CommonSection'
+// import 
+
 
 const Login = () => {
+
+  const loginNameRef = useRef()
+  const loginPasswordRef = useRef()
+  const submitHandler = e => {
+    e.preventDefault()
+  }
+
   return (
-    <div>Login</div>
+    <Helmet title='Login'>
+      <CommonSection title='Login' />
+      <section>
+        <Container>
+          <Row>
+            <Col lg='6' md='6' sm='12' className='m-auto text-center'>
+              <form action="" className="form mb-5"
+                onSubmit={submitHandler}
+              >
+
+                <div className="form__group">
+                  <input type="email" placeholder='Email' required
+                    ref={loginNameRef}
+                  />
+                </div>
+
+                <div className="form__group">
+                  <input type="password" placeholder='Password' required
+                    ref={loginPasswordRef}
+                  />
+                </div>
+
+                <button type="submit" className="addToCart__btn">Login</button>
+
+              </form>
+
+              <Link to='/register'>
+                Already have an account ? Create an account
+              </Link>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+
+    </Helmet>
   )
 }
 
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,8 +1,63 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import { Link } from 'react-router-dom'
+import { Col, Container, Row } from 'reactstrap'
+import Helmet from '../components/Helmet/Helmet'
+import CommonSection from '../components/UI/common-section/CommonSection'
+// import 
+
 
 const Register = () => {
+
+  const signupNameRef = useRef()
+  const signupPasswordRef = useRef()
+  const signEmailRef = useRef()
+
+  const submitHandler = e => {
+    e.preventDefault()
+  }
+
   return (
-    <div>Register</div>
+    <Helmet title='Signup'>
+      <CommonSection title='Signup' />
+      <section>
+        <Container>
+          <Row>
+            <Col lg='6' md='6' sm='12' className='m-auto text-center'>
+              <form action="" className="form mb-5"
+                onSubmit={submitHandler}
+              >
+
+                <div className="form__group">
+                  <input type="text" placeholder='Full Name' required
+                    ref={signupNameRef}
+                  />
+                </div>
+
+                <div className="form__group">
+                  <input type="email" placeholder='Email' required
+                    ref={signEmailRef}
+                  />
+                </div>
+
+                <div className="form__group">
+                  <input type="password" placeholder='Password' required
+                    ref={signupPasswordRef}
+                  />
+                </div>
+
+                <button type="submit" className="addToCart__btn">Sign Up</button>
+
+              </form>
+
+              <Link to='/register'>
+                Don't have an account ? Create an account
+              </Link>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+
+    </Helmet>
   )
 }
 
